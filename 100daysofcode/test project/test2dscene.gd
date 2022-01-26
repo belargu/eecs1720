85a099737a4a3370e2391b8a64f08bd0b8f574dc
patty <90358373+belargu@users.noreply.github.com>
2022-01-26 17:31:28 -0500

    worked on godot

---
+++ 100daysofcode/test project/test2dscene.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+"""
+var enemy = Vector2(5,5)
+var hero = Vector2(10,5)
+var direction: Vector2
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	"""
+	direction = hero - enemy
+	enemy += direction
+	print(enemy)
+	"""
+	
+	pass
+	
+	# pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
